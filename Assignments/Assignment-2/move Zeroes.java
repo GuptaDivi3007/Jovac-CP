77286d5f5b2a638342ce2be610bdde6f625ab3de
GuptaDivi3007 <122393384+GuptaDivi3007@users.noreply.github.com>
2024-06-20 14:59:13 +0530

    Update move Zeroes

--- Assignments/Assignment-2/move Zeroes.java
+++ Assignments/Assignment-2/move Zeroes.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                if(i!=index){
+                    int temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
+                }
+            index++;
+            }
+        }
+    }
+}
